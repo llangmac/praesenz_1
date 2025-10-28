cfb181a8cd7a325bc017a215dd513695485e6ea8
pixel <mr.lars@outlook.de>
2025-10-28 10:11:46 +0100

    P2 Commit

--- src/p2/P2_main.java
+++ src/p2/P2_main.java
@@ -1,12 +1,25 @@
 package p2;
 
+import java.util.random.RandomGenerator;
+
 public class P2_main {
 
     //Main
     public static void main(String[] args) {
 
+        //Assign random values to both variables
+        int i = RandomGenerator.getDefault().nextInt(20);
+        boolean b = RandomGenerator.getDefault().nextBoolean();
 
+        //Print originals values
+        System.out.println("\n" + "Initial value for int i: " + i);
+        System.out.println("Initial value for bool b: " + b);
+
+        //Check if i > 10
+        if(i>10) {b=true;}
+
+        //Print "updated" value of b
+        System.out.println("\n" + "Value of bool b: " + b);
 
     }
-
 }
