4bd3e4647baaea2ec0d4b4c072afd678334bac42
Lars <mr.lars@outlook.de>
2025-10-28 10:49:44 +0100

    Comments updated

--- src/p3/P3_main.java
+++ src/p3/P3_main.java
@@ -6,22 +6,25 @@ import java.util.random.RandomGeneratorFactory;
 
 public class P3_main {
 
+    //Main
     public static void main(String[] args) {
 
+        //Assign random values in Range 50 to i, j, k + print the output
         int i = RandomGenerator.getDefault().nextInt(50), j = RandomGenerator.getDefault().nextInt(50), k = RandomGenerator.getDefault().nextInt(50);
         System.out.println("\n" + "Initial values of i, j and k (in order): " + i + ", " + j + ", " + k);
 
+        //StringBuilder for Output
         StringBuilder m = new StringBuilder();
 
+        //Check defined expressions
         if(i<j) {m.append("A ");}
         if(i<k) {m.append("B ");}
         if(j<k) {m.append("C");}
 
+        //Case: StrinBuilder is empty => no expression applied!
         if(m.isEmpty()) {m.append("No expression applied! Empty output!");}
 
+        //Print m
         System.out.println(m);
-
-
     }
-
 }
